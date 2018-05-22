38c43beb98b05757b7667df818eac94ec125a34e
Cecile <ager89@yahoo.com.tw>
2018-05-22 20:54:07 +0800

    add post json

--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 "html/template"
 "net/http"
+"encoding/json"
+"log"
 )
 
-func indexHandle(w http.ResponseWriter, r *http.Request) {
-uploadTemplate := template.Must(template.ParseFiles("welcome.gtpl"))
-uploadTemplate.Execute(w, nil)
+type GPSdata struct {
+	Latitude string `json:"latitude,omitempty"`
+	Longitude string `json:"longitude,omitempty"`
 }
 
+var upload []GPSdata
 
+func indexHandle(w http.ResponseWriter, r *http.Request) {
+	uploadTemplate := template.Must(template.ParseFiles("welcome.gtpl"))
+	uploadTemplate.Execute(w, nil)
+}
+
+func CreateGPSEndpoint(w http.ResponseWriter, req *http.Request){
+	params := mux.Vars(req)
+	
+	json.NewEncoder(w).Encode(people)
+}
 
 func main() {
-http.HandleFunc("/", indexHandle)
-http.ListenAndServe(":9090", nil)
+	router := mux.NewRouter()
+	router.HandleFunc("/", indexHandle)
+	router.HandleFunc("/json", CreateGPSEndpoint).Methods("POST")
+	log.Fatal(http.ListenAndServe(":9090",router))
 }
